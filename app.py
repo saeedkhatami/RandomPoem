5559ffe2a9c24c9b98f261df7f4c4f76fe57dee3
0xb4dc0d3x <0xb4dc0d3x@gmail.com>
2022-09-14 02:21:15 +0430

    some changes to fix main function, faal added

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import requests as req
 from flask import Flask, render_template, send_file
-from take_screenshot import take_ss
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/poem/random?poetId=39024")
+@app.route('/', methods=['GET'])
+def random():
+    resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/poem/random")
     json_respond = resp.json()
-    html_respond = json_respond['htmlText']
     plain_text_respond = json_respond['plainText']
     new_plain_text = plain_text_respond.replace('\n', '<br>')
     poem_info = json_respond['fullTitle']
@@ -17,8 +15,13 @@ def main():
     return render_template('index.html', info=poem_info, poem=new_plain_text)
 
 
-@app.route('/photos')
-def show_image():
-    take_ss()
+@app.route('/faal', methods=['GET'])
+def faal():
+    resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/hafez/faal")
+    json_respond = resp.json()
+    plain_text_respond = json_respond['plainText']
+    new_plain_text = plain_text_respond.replace('\n', '<br>')
+    poem_info = json_respond['fullTitle']
 
-    return send_file("screenshots/screenshot.jpeg", mimetype='image/jpeg')
+    return render_template('index.html', info=poem_info, poem=new_plain_text)
+
