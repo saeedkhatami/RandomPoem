5bc4d7aec7beb6732d3185fe174faa45a644d937
0xb4dc0d3x <0xb4dc0d3x@gmail.com>
2022-09-14 02:34:06 +0430

    gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests as req
-from urllib.parse import urlencode
-from urllib.request import urlretrieve
-
-resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/hafez/faal")
-json_respond = resp.json()
-print(json_respond)
