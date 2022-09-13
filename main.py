85b7ba388e8ab04016d318f77203b34e32274495
0xb4dc0d3x <0xb4dc0d3x@gmail.com>
2022-09-13 13:21:36 +0430

    c

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
+import os
 import requests as req
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+from html2image import Html2Image
+
+photos_folder = os.path.join('static', 'photos')
+print(photos_folder)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = photos_folder
 
 
-# @app.route("/")
-# def hello_world():
-#     resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/poem/random")
-#     json_respond = resp.json()
-#     print(resp.json())
-#     print(json_respond['htmlText'])
-#     html_respond = json_respond['htmlText']
-#     return html_respond
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
     resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/poem/random?poetId=39024")
     json_respond = resp.json()
@@ -22,4 +18,21 @@ def main():
     plain_text_respond = json_respond['plainText']
     new_plain_text = plain_text_respond.replace('\n', '<br>')
     poem_info = json_respond['fullTitle']
+
     return render_template('index.html', info=poem_info, poem=new_plain_text)
+
+
+def take_photo():
+    hti = Html2Image(output_path=photos_folder)
+    hti.screenshot(
+        url='http://127.0.0.1:5000/',
+        save_as='preview.png',
+        size=(500, 650)
+    )
+
+
+@app.route('/photos')
+def show_image():
+    take_photo()
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'preview.png')
+    return send_file(full_filename, mimetype='image/png')
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 import requests as req
-
 resp = req.request(method='GET', url="https://ganjgah.ir/api/ganjoor/poem/random")
+print(resp)
 json_respond = resp.json()
 html_respond = json_respond['htmlText']
 plain_text_respond = json_respond['plainText']
-verses = json_respond['verses']
-
-for i in verses:
-    print(i['text'])
-
-print(json_respond)
-print(json_respond['fullTitle'])
-print(type(plain_text_respond))
